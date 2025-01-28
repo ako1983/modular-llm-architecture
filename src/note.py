9d6689a147a997e4b4d2ba92186fa1c0e148cd5e
Ako <ako1983@users.noreply.github.com>
2025-01-27 21:24:33 -0800

    Update note.py

--- src/note.py
+++ src/note.py
@@ -1,7 +1,1 @@
-# this is a place holder for note taking agent
-from logging_config import setup_logger
 
-# Set up the logger for this module
-logger = setup_logger(__name__)
-
-logger.info("This is an info log from the current module.")
